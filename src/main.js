15d2182e95e440d3b27e3f0ea1b51bf062078850
Tony Mogoa <mogoa.tonny@gmail.com>
2021-06-18 09:04:02 +0300

    integrated controls into the window

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-var vid,playbtn,seekslider, curtimetext, durtimetext, mutebtn , volumeslider , fullscreenbtn;
+var vid,
+    playbtn,
+    seekslider,
+    curtimetext,
+    durtimetext,
+    mutebtn,
+    volumeslider,
+    fullscreenbtn,
+    videoControlsBar;
 
-  function initializePlayer(){
+function initializePlayer() {
     //Set object references
-    vid = document.getElementById("my_video");
+    vid = document.querySelector("video");
+    console.log(vid);
     playbtn = document.getElementById("playpausebtn");
     seekslider = document.getElementById("seekslider");
     curtimetext = document.getElementById("curtimetext");
@@ -10,63 +19,86 @@ var vid,playbtn,seekslider, curtimetext, durtimetext, mutebtn , volumeslider , f
     mutebtn = document.getElementById("mutebtn");
     volumeslider = document.getElementById("volumeslider");
     fullscreenbtn = document.getElementById("fullscreenbtn");
+    videoControlsBar = document.getElementById("video_controls_bar");
 
     //Add event listeners
-    playbtn.addEventListener("click",playPause,false);
-    seekslider.addEventListener("change",vidSeek,false);
-    vid.addEventListener("timeupdate",seektimeupdate,false);
-    mutebtn.addEventListener("click",vidmute,false);
-    volumeslider.addEventListener("change",setvolume,false);
-    fullscreenbtn.addEventListener("click",toggleFullScreen,false);
-  }
-  window.onload = initializePlayer;
+    playbtn.addEventListener("click", playPause, false);
+    seekslider.addEventListener("change", vidSeek, false);
+    vid.addEventListener("timeupdate", seektimeupdate, false);
+    mutebtn.addEventListener("click", vidmute, false);
+    volumeslider.addEventListener("change", setvolume, false);
+    fullscreenbtn.addEventListener("click", toggleFullScreen, false);
+    console.log("height" + videoControlsBar.offsetHeight);
+    vid.addEventListener(
+        "loadedmetadata",
+        function (e) {
+            console.log("video " + this.videoWidth);
+            ipcRenderer.send(
+                "window:resize",
+                (800 * this.videoHeight) / this.videoWidth +
+                    videoControlsBar.offsetHeight
+            );
+        },
+        false
+    );
+}
+window.onload = initializePlayer;
 
-    function playPause(){
-
-      if (vid.paused) {
+function playPause() {
+    if (vid.paused) {
         vid.play();
         playbtn.innerHTML = "Pause";
-      }else {
+    } else {
         vid.pause();
         playbtn.innerHTML = "Play";
-      }
     }
-    function vidSeek(){
-      var seekto = vid.duration * (seekslider.value / 100);
-      vid.currentTime = seekto;
+}
+function vidSeek() {
+    var seekto = vid.duration * (seekslider.value / 100);
+    vid.currentTime = seekto;
+}
+function seektimeupdate() {
+    var nt = vid.currentTime * (100 / vid.duration);
+    seekslider.value = nt;
+    var curmins = Math.floor(vid.currentTime / 60);
+    var cursecs = Math.floor(vid.currentTime - curmins * 60);
+    var durmins = Math.floor(vid.duration / 60);
+    var dursecs = Math.floor(vid.duration - durmins * 60);
+    if (cursecs < 10) {
+        cursecs = "0" + cursecs;
     }
-    function seektimeupdate(){
-      var nt = vid.currentTime * (100 / vid.duration);
-      seekslider.value = nt;
-      var curmins = Math.floor(vid.currentTime / 60);
-    	var cursecs = Math.floor(vid.currentTime - curmins * 60);
-    	var durmins = Math.floor(vid.duration / 60);
-    	var dursecs = Math.floor(vid.duration - durmins * 60);
-    	if(cursecs < 10){ cursecs = "0"+cursecs; }
-    	if(dursecs < 10){ dursecs = "0"+dursecs; }
-    	if(curmins < 10){ curmins = "0"+curmins; }
-    	if(durmins < 10){ durmins = "0"+durmins; }
-    	curtimetext.innerHTML = curmins+":"+cursecs;
-    	durtimetext.innerHTML = durmins+":"+dursecs
+    if (dursecs < 10) {
+        dursecs = "0" + dursecs;
     }
-    function vidmute() {
-      if (vid.muted) {
+    if (curmins < 10) {
+        curmins = "0" + curmins;
+    }
+    if (durmins < 10) {
+        durmins = "0" + durmins;
+    }
+    curtimetext.innerHTML = curmins + ":" + cursecs;
+    durtimetext.innerHTML = durmins + ":" + dursecs;
+}
+function vidmute() {
+    if (vid.muted) {
         vid.muted = false;
         mutebtn.innerHTML = "Mute";
-      }else {
+    } else {
         vid.muted = true;
         mutebtn.innerHTML = "Unmute";
-      }
     }
-    function setvolume(){
-      vid.volume = volumeslider.value /100 ;
-    }
-    function toggleFullScreen(){
-      if (vid.requestFullScreen) {
+}
+function setvolume() {
+    vid.volume = volumeslider.value / 100;
+}
+function toggleFullScreen() {
+    if (vid.requestFullScreen) {
         vid.requestFullScreen();
-      }if (vid.webkitRequestFullScreen) {
-        vid.webkitRequestFullScreen();
-      }if (vid.mozRequestFullScreen) {
-        vid.mozRequestFullScreen();
-      }
     }
+    if (vid.webkitRequestFullScreen) {
+        vid.webkitRequestFullScreen();
+    }
+    if (vid.mozRequestFullScreen) {
+        vid.mozRequestFullScreen();
+    }
+}
--- src/render.js
+++ src/render.js
@@ -4,18 +4,18 @@ const { ipcRenderer } = require("electron");
 const getWindow = () => remote.BrowserWindow.getFocusedWindow();
 
 const closeBtn = document.getElementById("close");
-const vid = document.querySelector("video");
-vid.addEventListener(
-    "loadedmetadata",
-    function (e) {
-        console.log("video " + this.videoWidth);
-        ipcRenderer.send(
-            "window:resize",
-            (800 * this.videoHeight) / this.videoWidth
-        );
-    },
-    false
-);
+//const vid = document.querySelector("video");
+// vid.addEventListener(
+//     "loadedmetadata",
+//     function (e) {
+//         console.log("video " + this.videoWidth);
+//         ipcRenderer.send(
+//             "window:resize",
+//             (800 * this.videoHeight) / this.videoWidth
+//         );
+//     },
+//     false
+// );
 
 //ipcRenderer.send("window:resize", vid.videoHeight);
 
