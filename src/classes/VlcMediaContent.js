40dd1af6b2876157defdc31dbe6a4ba3bfa49554
levizwannah <levizwannah@gmail.com>
2021-07-01 13:55:19 +0300

    started the Media content class

--- src/classes/VlcMediaContent.js
+++ src/classes/VlcMediaContent.js
@@ -1,3 +1,16 @@
 /**
  * The VLC media content class
+ * This class is the parent of the Vlc video and audio class
  */
+
+class VLCMediaContent{
+
+    /**
+     * Variables that will be used by both the audio and video classes
+     */
+
+    play(){
+
+    }
+
+}
