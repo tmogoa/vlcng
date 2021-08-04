4dd9ecd683cc36d9ca3cf59ea5deae5574557b82
Tony Mogoa <mogoa.tonny@gmail.com>
2021-08-04 21:10:26 +0300

    Option menu done

---
+++ src/components/contextMenu.js
@@ -0,0 +1,59 @@
+const contextMenu = document.getElementById("context-menu");
+const scope = document.querySelector("body");
+
+const normalizePozition = (mouseX, mouseY) => {
+    // ? compute what is the mouse position relative to the container element (scope)
+    let { left: scopeOffsetX, top: scopeOffsetY } =
+        scope.getBoundingClientRect();
+
+    scopeOffsetX = scopeOffsetX < 0 ? 0 : scopeOffsetX;
+    scopeOffsetY = scopeOffsetY < 0 ? 0 : scopeOffsetY;
+
+    const scopeX = mouseX - scopeOffsetX;
+    const scopeY = mouseY - scopeOffsetY;
+
+    // ? check if the element will go out of bounds
+    const outOfBoundsOnX = scopeX + contextMenu.clientWidth > scope.clientWidth;
+
+    const outOfBoundsOnY =
+        scopeY + contextMenu.clientHeight > scope.clientHeight;
+
+    let normalizedX = mouseX;
+    let normalizedY = mouseY;
+
+    // ? normalize on X
+    if (outOfBoundsOnX) {
+        normalizedX =
+            scopeOffsetX + scope.clientWidth - contextMenu.clientWidth;
+    }
+
+    // ? normalize on Y
+    if (outOfBoundsOnY) {
+        normalizedY =
+            scopeOffsetY + scope.clientHeight - contextMenu.clientHeight;
+    }
+
+    return { normalizedX, normalizedY };
+};
+
+scope.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+
+    const { clientX: mouseX, clientY: mouseY } = event;
+    const { normalizedX, normalizedY } = normalizePozition(mouseX, mouseY);
+
+    contextMenu.style.top = `${normalizedY}px`;
+
+    contextMenu.style.left = `${normalizedX}px`;
+
+    contextMenu.classList.remove("hide-menu");
+    setTimeout(() => {
+        contextMenu.classList.add("show-menu");
+    });
+});
+
+scope.addEventListener("click", (e) => {
+    if (e.target.offsetParent != contextMenu) {
+        contextMenu.classList.add("hide-menu");
+    }
+});
